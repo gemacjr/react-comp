c8f820a73756c096b84759f6458d658a5751a349
Eddie <eddie@Eddies-iMac.local>
2018-06-25 15:43:11 -0600

    More components

---
+++ src/docs/examples/TextInputCssModules/Example.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import TextInputCssModules from 'ps-react/TextInputCssModules';
+
+/**
+ * With CSS Modules
+ */
+export default class Example extends React.Component {
+  render() {
+    return (
+      <div>
+
+        <TextInputCssModules
+          htmlId="text-input-css-modules-with-error"
+          label="With Error"
+          name="firstname"
+          onChange={() => {}}
+          required
+          error="First name is required."
+        />
+      </div>
+    )
+  }
+}
